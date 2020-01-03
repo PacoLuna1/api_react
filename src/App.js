f6da85ba612bd7c067f395f9f1820f719ade78ca
Paco Luna <pakoo_15@hotmail.com>
2020-01-03 14:53:04 -0600

    Add api fetch functionality

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			movies: []
+		};
+	}
+	componentDidMount() {
+		fetch('/api/api_movies')
+			.then((response) => response.json())
+			.then((json) => this.setState({ movies: json }))
+			.catch((error) => console.log(error));
+	}
+
+	renderMovies() {
+		return this.state.movies.map((movie) => <h1>{movie.title}</h1>);
+	}
+
+	render() {
+		return <div className="App">{this.renderMovies()}</div>;
+	}
 }
 
 export default App;
